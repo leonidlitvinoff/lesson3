0401c05cd17f5e74c2b15e19d3cb25e4e0cac69c
Leonid Litvinov <leonidlitvinoff@yandex.ru>
2019-11-16 20:58:02 +0700

    Commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import sqlite3
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidgetItem
 from design import Ui_Form
-
 
 
 class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.printer)
+        self.pushButton.clicked.connect(self.adder)
 
-    def printer(self):
-        text = ', '.join([self.buttonGroup.checkedButton().text(), self.buttonGroup_2.checkedButton().text(),
-                          self.buttonGroup_3.checkedButton().text()])
-        self.label.setText(text)
+    def adder(self):
+        if self.lineEdit.text() == '' or self.lineEdit_2.text() == '':
+            self.label_3.setText('Error')
+        else:
+            text = [self.lineEdit.text(), self.lineEdit_2.text()]
+            self.listWidget.addItem(QListWidgetItem('\t'.join(text)))
 
 
 app = QApplication(sys.argv)
